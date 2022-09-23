e27bcc44161c8b4fd69f401f0ea7fa63ba120c4c
Fabio de Sousa Leal <fsl2@msn.com>
2022-09-23 16:53:44 -0300

    Jogo de Adivinhacao - treinando laco while

--- python-comencando-linguagem/adivinhacao.py
+++ python-comencando-linguagem/adivinhacao.py
@@ -1,24 +1,30 @@
 print("**********Bem vindo ao jogo de adivinhação *************")
 
 numero_secreto=42
+total_tentativa=3
+rodada=1
 
-chute_str=input("Digite o seu número: ")
+while (rodada <= total_tentativa):
+    print("Tentativa {} de {}".format(rodada, total_tentativa))
+    chute_str=input("Digite o seu número: ")
 
-print("Você digitou: ", chute_str)
+    print("Você digitou: ", chute_str)
 
-chute=int(chute_str)
+    chute=int(chute_str)
 
-acertou = chute==numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute==numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(acertou):
-    print("Você acertou!")
+    if(acertou):
+        print("Você acertou!")
 
-else:
-    if(maior):
-        print("Você errou! Seu chute foi maior que o número secreto")
-    elif(menor):
-        print("Você errou! Seu chute foi menor que o número secreto")
+    else:
+        if(maior):
+            print("Você errou! Seu chute foi maior que o número secreto")
+        elif(menor):
+            print("Você errou! Seu chute foi menor que o número secreto")
+
+    rodada = rodada+1
 
 print("Fim de Jogo")
