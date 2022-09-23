e0910cd495b685f569f2f262ecf2ab9a30328fc3
Fabio de Sousa Leal <fsl2@msn.com>
2022-09-23 11:00:28 -0300

    Jogo de Adivinhacao - if e else

--- python-comencando-linguagem/adivinhacao.py
+++ python-comencando-linguagem/adivinhacao.py
@@ -1,5 +1,24 @@
 print("**********Bem vindo ao jogo de adivinhação *************")
 
 numero_secreto=42
+
 chute_str=input("Digite o seu número: ")
+
 print("Você digitou: ", chute_str)
+
+chute=int(chute_str)
+
+acertou = chute==numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if(acertou):
+    print("Você acertou!")
+
+else:
+    if(maior):
+        print("Você errou! Seu chute foi maior que o número secreto")
+    elif(menor):
+        print("Você errou! Seu chute foi menor que o número secreto")
+
+print("Fim de Jogo")
